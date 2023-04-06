a71ef165eb36727953532fd1f0bdb3e17d675844
webees <5155291+webees@users.noreply.github.com>
2023-04-06 10:15:05 +0800

    add LSTM

--- my.py
+++ my.py
@@ -11,36 +11,33 @@ class MyTuner(kt.Hyperband):
 
 
 class MyHyperModel(kt.HyperModel):
-    def __init__(self, learning_rate, input_units, input_activ, output_units, output_activ, dense_nums, dense_units, dense_activ, compile_loss, compile_metrics):
+    def __init__(self, learning_rate, input, output, depth, lstm,  dense, compile):
         self.learning_rate = learning_rate
-        self.input_units = input_units
-        self.input_activ = input_activ
-        self.output_units = output_units
-        self.output_activ = output_activ
-        self.dense_nums = dense_nums
-        self.dense_units = dense_units
-        self.dense_activ = dense_activ
-        self.compile_loss = compile_loss
-        self.compile_metrics = compile_metrics
+        self.input = input
+        self.output = output
+        self.depth = depth
+        self.lstm = lstm
+        self.dense = dense
+        self.compile = compile
 
     def build(self, hp):
-        '''
-        Write a function that creates and returns a Keras model. Use the hp argument to define the hyperparameters during model creation.
-        ['relu', 'sigmoid', 'softmax', 'softplus', 'softsign', 'tanh', 'selu', 'elu', 'exponential', 'leaky_relu', 'swish', 'exponential', 'hard_sigmoid', 'linear']
-        '''
-        hp_dense_nums = hp.Int("dense_nums", min_value=self.dense_nums[0], max_value=self.dense_nums[1], step=self.dense_nums[2])
+        '''['relu', 'sigmoid', 'softmax', 'softplus', 'softsign', 'tanh', 'selu', 'elu', 'exponential', 'leaky_relu', 'swish', 'exponential', 'hard_sigmoid', 'linear']'''
+        hp_depth = hp.Int("depth", min_value=self.depth[0], max_value=self.depth[1], step=self.depth[2])
         hp_lr = hp.Choice("learning_rate", values=self.learning_rate)
         model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Dense(units=self.input_units, activation=self.input_activ))  # Intput layer.
-        for i in range(hp_dense_nums):  # Number of layers of the MLP is a hyperparameter.
-            _units = hp.Int(f"units_{i}", min_value=self.dense_units[0], max_value=self.dense_units[1], step=self.dense_units[2])  # Number of units of each layer are different hyperparameters with different names.
-            _activ = hp.Choice(f"activ_{i}", self.dense_activ)
-            model.add(tf.keras.layers.Dense(units=_units, activation=_activ))
-        model.add(tf.keras.layers.Dense(units=self.output_units, activation=self.output_activ))  # Output layer.
+        model.add(tf.keras.layers.Dense(units=self.input[0], activation=self.input[1]))
+        for i in range(hp_depth):
+            lstm_units = hp.Int(f"lstm_{i}", min_value=self.lstm[0], max_value=self.lstm[1], step=self.lstm[2])
+            lstm_activ = hp.Choice(f"lstm_{i}", self.lstm[3])
+            dense_units = hp.Int(f"units_{i}", min_value=self.dense[0], max_value=self.dense[1], step=self.dense[2])
+            dense_activ = hp.Choice(f"activ_{i}", self.dense[3])
+            model.add(tf.keras.layers.LSTM(units=lstm_units, activation=lstm_activ))
+            model.add(tf.keras.layers.Dense(units=dense_units, activation=dense_activ))
+        model.add(tf.keras.layers.Dense(units=self.output[0], activation=self.output[1]))
         model.compile(
-            optimizer=tf.keras.optimizers.Adam(hp_lr),  # Automatically chooses learning rate.
-            loss=self.compile_loss,
-            metrics=self.compile_metrics,
+            optimizer=tf.keras.optimizers.Adam(hp_lr),
+            loss=self.compile[0],
+            metrics=self.compile[1],
             run_eagerly=True
         )
         return model
